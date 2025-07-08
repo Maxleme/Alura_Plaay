ec33f71180e8163ea5946e91c98898c3ffeea2d3
Max <maxleme_21@hotmail.com>
2025-07-08 14:33:50 -0300

    configurada sessão para armazenamento do estado de logado do usuario, também foi configurada as autorizações de rotas e redirecionamentos caso esteja logado e caso não esteja, também foi configurado rota e botão para desfazer a sessão

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+namespace Alura\Mvc\Controller;
+
+class LogoutController implements Controller
+{
+    public function processaRequisicao(): void
+    {
+        unset($_SESSION['logado']);
+        header("Location: /login");
+    }
+}
